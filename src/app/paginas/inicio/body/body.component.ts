ff91f08400f69be342265ccac3e99c8dad128b40
Mitwar99 <mitwarhuacan@hotmail.com>
2025-02-26 10:38:26 -0500

    Agregar actualizaciones

--- src/app/paginas/inicio/body/body.component.ts
+++ src/app/paginas/inicio/body/body.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { QuienesSomosComponent } from '../../sub-paginas/quienes-somos/quienes-somos.component';
+import { MisCursosComponent } from '../../sub-paginas/mis-cursos/mis-cursos.component';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.scss'],
   imports: [
-    QuienesSomosComponent,
-                BodyComponent
+              QuienesSomosComponent,
+             MisCursosComponent
     ],
   
 })
--- src/app/paginas/sub-paginas/mis-cursos/mis-cursos.component.ts
+++ src/app/paginas/sub-paginas/mis-cursos/mis-cursos.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
+import { ProductService } from '../../../../service/productservice';
+import { Product } from '../../../../domain/product';
 
 @Component({
   selector: 'app-mis-cursos',
+  standalone: true,
   templateUrl: './mis-cursos.component.html',
-  styleUrls: ['./mis-cursos.component.scss']
+  styleUrls: ['./mis-cursos.component.scss'],
+  imports:[ CarouselModule, ButtonModule,
+            TagModule
+  ],
+  providers: [ProductService]
 })
 export class MisCursosComponent implements OnInit {
+  products: Product[] = [];
+  responsiveOptions: any[] | undefined;
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.productService.getProductsSmall().then((products) => {
+      this.products = products;
+  });
+
+  this.responsiveOptions = [
+      {
+          breakpoint: '1400px',
+          numVisible: 3,
+          numScroll: 3
+      },
+      {
+          breakpoint: '1220px',
+          numVisible: 2,
+          numScroll: 2
+      },
+      {
+          breakpoint: '1100px',
+          numVisible: 1,
+          numScroll: 1
+      }
+  ];
   }
+  getSeverity(status: string){
+    switch(status){
+      case 'INSTOCK':
+        const nn = 'succes'
+        console.log("hola", nn);
+      break
+    }
+  }
+//   getSeverity(status: string) {
+//     switch (status) {
+//         case 'INSTOCK':
+//             return 'success';
+//         case 'LOWSTOCK':
+//             return 'warning';
+//         case 'OUTOFSTOCK':
+//             return 'danger';
+//     }
+// }
 
 }
