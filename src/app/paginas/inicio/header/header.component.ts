ae23af754da3254fef51e7d7109972c5cc0dc3c5
Mitwar99 <mitwarhuacan@hotmail.com>
2025-02-27 10:39:19 -0500

    Agregar slider a la nav

--- src/app/paginas/inicio/header/header.component.ts
+++ src/app/paginas/inicio/header/header.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, model } from '@angular/core';
+import { GalleriaModule } from 'primeng/galleria';
+import { PhotoService } from '../../../../service/photoservice';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [],
-  providers:[]
+  imports: [ GalleriaModule ],
+  providers:[PhotoService]
 })
 export class HeaderComponent implements OnInit {
+  images: any[] = [];
 
-  constructor() { }
+    responsiveOptions: any[] = [
+        {
+            breakpoint: '1300px',
+            numVisible: 4
+        },
+        {
+            breakpoint: '575px',
+            numVisible: 1
+        }
+    ];
+
+  constructor(private photoService: PhotoService) { }
   
   ngOnInit() {
-  }
+    this.photoService.getImages().then((images) => {
+      this.images = images; // Asignación correcta
+    });
+}
 
 }
